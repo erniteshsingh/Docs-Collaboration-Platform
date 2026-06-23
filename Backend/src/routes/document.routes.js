93b16b0d239d7f683df6bce74010f04a306f3ba9
niteshsingh <er.niteshkrsingh@gmail.com>
2026-06-23 11:28:49 +0530

    Code update

--- Backend/src/routes/document.routes.js
+++ Backend/src/routes/document.routes.js
@@ -13,7 +13,7 @@ import {
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { checkDocumentPermission } from "../middlewares/checkDocumentationPermission.js";
 
-router.post("/", authMiddleware, createDocument);
+router.post("/",createDocument);
 
 router.get("/", authMiddleware, getAllDocuments);
 
